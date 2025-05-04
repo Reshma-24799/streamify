87986c4daa292aad3f54e7c9eaf4d239a596022c
RESHMA R <reshma24799@gmail.com>
2025-05-04 16:41:54 +0200

    login , logout endpoints setup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
@@ -33,6 +34,17 @@ export async function signup(req,res) {
             profilePic: randomAvatar
         });
 
+        try {
+            await upsertStreamUser({
+              id: newUser._id.toString(),
+              name: newUser.fullName,
+              image: newUser.profilePic || "",
+            });
+            console.log(`Stream user created for ${newUser.fullName}`);
+        } catch (error) {
+            console.log("Error creating Stream user:", error);
+        }
+
         const token = jwt.sign({userId: newUser._id }, process.env.JWT_SECRET_KEY, {
             expiresIn: "7d"
         });
@@ -52,8 +64,36 @@ export async function signup(req,res) {
 }
 
 export async function login(req,res) {
-    res.send("login route is working");
+    try{
+        const {email, password} = req.body;
+        if(!email || !password ) {
+            return res.status(400).json({message: "Please fill all the fields"});
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({message: "Invalid email or password"})
+        }
+        const isPasswordCorrect = await user.matchPassword(password, user.password)
+        if(!isPasswordCorrect) {
+            return res.status(401).json({message: "Invalid email or password"})
+        }
+        const token = jwt.sign({userId: user._id }, process.env.JWT_SECRET_KEY, {
+            expiresIn: "7d"
+        });
+        res.cookie("jwt",token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production",
+        });
+        res.status(200).json({success: true, user})
+
+    } catch (error) {
+        console.log("Error in login controller: ", error);
+        res.status(500).json({message: "Internal server error"});
+    }
 }
 export async function logout(req,res) {
-    res.send("logout route is working");
+    res.clearCookie("jwt");
+    res.status(200).json({success: true, message: "Logged out successfully"})
 }
